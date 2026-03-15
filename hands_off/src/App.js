b6129fe3aeae1b3e80fe1a26c252a9b1a6ad0883
Nhan-DH <hongnhan474747@gmail.com>
2026-03-15 11:43:33 +0700

    build UI frame

--- hands_off/src/App.js
+++ hands_off/src/App.js
@@ -3,10 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hands off App
-      </header>
+    <div className="main">
+
+      <h1 style={{ fontSize: "40px" }}>Hands off App</h1>
+      <video
+        className="video"
+        autoPlay
+      />
+      <div className="control">
+        <button>Train 1</button>
+        <button>Train 2</button>
+        <button>Run</button>
+      </div>
+
+
+
     </div>
   );
 }
